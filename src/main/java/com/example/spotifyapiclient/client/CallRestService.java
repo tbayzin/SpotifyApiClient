2006d3e2387744a2274844cc439c16b175648a1a
tarikb <bayzin@hotmail.com.tr>
2021-09-13 17:26:11 +0300

    ,

--- src/main/java/com/example/spotifyapiclient/client/CallRestService.java
+++ src/main/java/com/example/spotifyapiclient/client/CallRestService.java
@@ -5,15 +5,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+
 public class CallRestService implements CommandLineRunner {
 
     private static void CallRestService () {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        @RequestMapping("/")
-
-
+        HashMap<String, Object> planet = restTemplate.getForObject ("https://api.spotify.com/v1/artists/06HL4z0CvFAxyc27GXpf02",HashMap.class);
 
 
 
